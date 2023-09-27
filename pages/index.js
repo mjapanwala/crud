3716214bdd6a21b2d2a74dd382a232be6938c663
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2023-09-27 01:53:12 -0400

    everything

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { getFileInformation } from "../../posts/getData";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+  const [updateText, setUpdateText] = useState({show:false, indexNumber:undefined});
+  const [changeText, setChangeText] = useState('')
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (list.length === 0) {
+      setList([text]);
+    } else {
+      setList((prev) => {
+        return [...prev, text];
+      });
+    }
+  }
+
+  function handleDelete(listItem, indexNumber) {
+    const removeDuplicate = list.filter((textItem, index) => {
+      if (textItem === listItem && indexNumber === index) {
+        return false;
+      }
+      return true;
+    });
+    setList(removeDuplicate);
+  }
+
+
+
+  function changeHandler(e, indexNumber) {
+   
+    e.preventDefault()
+    const newList = [...list];
+    newList[indexNumber] = changeText
+    setList(newList);
+    
+  }
+
+ 
   return (
     <div className={styles.container}>
       <Head>
@@ -10,43 +50,58 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <form
+          onSubmit={(e) => {
+            handleClick(e);
+          }}
+        >
+          <h1>Your friendly writing note here</h1>
+          <label htmlFor="Heres your input to add to your list" />
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            name="newItem"
+            type="text"
+          />
+          <button type="submit">Add</button>
+        </form>
+        {list.length > 0 && (
+          <>
+            <h4>Here is your list of items below</h4>
+            {list.map((listItem, indexNumber) => {
+              return (
+                <>
+                  <button key={indexNumber} className={styles.list}>
+                    {listItem}
+                  </button>
+                  <div>
+                    <div>
+                      <span className={styles.rowGang} onClick={() => handleDelete(listItem, indexNumber)}>
+                        Remove Item
+                      </span>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                      <span className={styles.rowGang} onClick={() => setUpdateText({show: true, indexNumber})}>
+                        Update Item
+                      </span>
+                    </div>
+                    {updateText.show && updateText.indexNumber === indexNumber && (
+                        <>
+                        <form>
+                          <label htmlFor="updateText">Update Text</label>
+                          <input
+                            type="text"             
+                            onChange={(e) => setChangeText(e.target.value)}
+                          />
+                          <button type="submit" onClick={(e) =>changeHandler(e, indexNumber)}> Change </button>
+                        </form>
+                        </>
+                      )}
+                  </div>
+                </>
+              );
+            })}
+          </>
+        )}
       </main>
 
       <footer>
@@ -55,7 +110,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -92,15 +147,8 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
@@ -109,17 +157,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
         * {
@@ -129,3 +168,5 @@ export default function Home() {
     </div>
   );
 }
+
+
